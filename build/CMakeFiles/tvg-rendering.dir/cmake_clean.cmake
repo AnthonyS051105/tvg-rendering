880fd41887bdfee78433e67719e7b11465600417
AnthonyS051105 <yanthonysaputra58@gmail.com>
2026-05-22 16:12:34 +0700

    add: material png implementation

---
+++ build/CMakeFiles/tvg-rendering.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tvg-rendering.dir/src/main.cpp.o"
+  "CMakeFiles/tvg-rendering.dir/src/main.cpp.o.d"
+  "tvg-rendering"
+  "tvg-rendering.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tvg-rendering.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/tvg-rendering.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for tvg-rendering.
+# This may be replaced when dependencies are built.
